73d63fda20c820c919c53054a5ba9e68f298de57
Mohammad Firdaus <firdsm@gmail.com>
2019-10-01 07:03:37 +0700

    Update install.sh
    add echo hastag wkwk

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "####################################################"
 echo "#~Script to install Spotify on Manjaro via flatpak~#"
+echo "####################################################"
 sudo pacman -Sy
 pamac install flatpak
 flatpak install flathub com.spotify.Client
